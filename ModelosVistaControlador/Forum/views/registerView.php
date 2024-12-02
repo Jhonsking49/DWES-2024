41d630b5aa7b66641c713a6e177baf6918333e7d
Jhonsking49 <juanreygonzalez2002@gmail.com>
2024-12-02 01:12:32 +0100

    1-12-2024

--- ModelosVistaControlador/Forum/views/registerView.php
+++ ModelosVistaControlador/Forum/views/registerView.php
@@ -8,13 +8,6 @@
 </head>
 
 <body>
-    <?php
-    if (isset($_SESSION['info'])) {
-        echo "<script>alert('" . $_SESSION['info'] . "')</script>";
-
-        unset($_SESSION['info']); // elimina la variable de sesión
-    }
-    ?>
     <h1>Registrar nuevo usuario</h1>
     <form action="index.php?c=user?register=1" method="post">
         <input type="text" id="name" name="username" required placeholder="nombre usuario"><br><br>
